6053a3521d762afdc6469640ea4946204c505915
Nevin <nevinpuri1901@gmail.com>
2023-01-16 14:28:13 +0100

    implemented operators

--- src/value.rs
+++ src/value.rs
@@ -39,6 +39,18 @@ where
     }
 }
 
+impl<T> ops::Sub<T> for Value<T>
+where
+    T: Float + ops::SubAssign,
+{
+    type Output = Value<T>;
+    fn sub(self, rhs: T) -> Self::Output {
+        Value {
+            data: self.data - rhs
+        }
+    }
+}
+
 impl<T> ops::Mul<T> for Value<T>
 where
     T: Float + ops::MulAssign,
@@ -47,6 +59,18 @@ where
     fn mul(self, rhs: T) -> Self::Output {
         Value {
             data: self.data * rhs,
+        }
+    }
+}
+
+impl<T> ops::Div<T> for Value<T>
+where
+    T: Float + ops::DivAssign
+{
+    type Output = Value<T>;
+    fn div(self, rhs: T) -> Self::Output {
+        Value {
+            data: self.data / rhs
         }
     }
 }
@@ -63,6 +87,18 @@ where
     }
 }
 
+impl<T> ops::Sub<Value<T>> for Value<T>
+where
+    T: Float,
+{
+    type Output = Value<T>;
+    fn sub(self, rhs: Value<T>) -> Self::Output {
+        Value {
+            data: self.data - rhs.data
+        }
+    }
+}
+
 impl<T> ops::Mul<Value<T>> for Value<T>
 where
     T: Float,
@@ -71,6 +107,18 @@ where
     fn mul(self, rhs: Value<T>) -> Self::Output {
         Value {
             data: self.data * rhs.data,
+        }
+    }
+}
+
+impl<T> ops::Div<Value<T>> for Value<T>
+where
+    T: Float,
+{
+    type Output = Value<T>;
+    fn div(self, rhs: Value<T>) -> Self::Output {
+        Value {
+            data: self.data / rhs.data
         }
     }
 }
