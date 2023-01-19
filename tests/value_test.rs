2fe00fbfb24c8e75ad63174d1409318faa338144
Nevin1901 <nevinpuri1901@gmail.com>
2023-01-19 18:00:09 +0100

    derivative

--- tests/value_test.rs
+++ tests/value_test.rs
@@ -1,22 +1,45 @@
 use rsgrad::value::Value;
 
 #[test]
-fn test_value() {
+fn test_grad() {
+    let h = 0.00001;
+
     let a: Value<f32> = Value::with_label(2.0, "a");
     let b: Value<f32> = Value::with_label(-3.0, "b");
     let c: Value<f32> = Value::with_label(10.0, "c");
 
     let mut e = a * b;
     e.label = "e";
-
     let d = e + c;
-
     let f = Value::with_label(-2.0, "f");
 
-    let mut L = d * f;
-    L.label = "l";
+    let mut l = d * f;
+    l.label = "l";
+    let l1 = l.data;
 
-    println!("{:#?}", L);
+    // we're adding h in this second part
+    let a: Value<f32> = Value::with_label(2.0, "a") + h;
+    let b: Value<f32> = Value::with_label(-3.0, "b");
+    let c: Value<f32> = Value::with_label(10.0, "c");
+
+    let mut e = a * b;
+    e.label = "e";
+    let d = e + c;
+    let f = Value::with_label(-2.0, "f");
+    let mut l = d * f;
+    l.label = "l";
+
+    let l2 = l.data;
+
+    let derivative = (l2 - l1) / h;
+
+    println!("{:.2}", (l2 - l1) / h);
+    assert_eq!(format!("{:.2}", derivative), "6.01");
+
+    // calculating derivatives
+    // L
+    // what is the derivative of L with respect to L
+
     // assert_eq!(d.data, 4.0)
     // trace_console(d);
 
